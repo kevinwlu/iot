b1c8adac8c0d2494ff5ae519932f0d20953cbd23
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-01-10 23:06:18 -0500

    Update pub.py

--- lesson5/pub.py
+++ lesson5/pub.py
@@ -1,4 +1,2 @@
 import paho.mqtt.publish as publish
-publish.single("paho/test/single", "Hello", hostname="localhost")
-msgs = [{'topic':"paho/test/multiple", 'payload':"Hello 1"}, ("paho/test/multiple", "Hello 2", 0, False)]
-publish.multiple(msgs, hostname="localhost")
+publish.single("paho/test", "Hello", hostname="localhost")
