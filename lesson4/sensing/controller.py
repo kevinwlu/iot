4679ec0b3984280b054818ad8ba3a5ddde1ca6bb
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-02-15 20:09:45 -0500

    Create controller.py

---
+++ lesson4/sensing/controller.py
@@ -0,0 +1,55 @@
+# Python code for controller native service -
+# smart sensing - controller.py
+
+import RPi.GPIO as GPIO
+import time
+import sys
+import requests
+
+GPIO.setmode(GPIO.BCM)
+ROOM_SENSOR_PIN = 27
+DOOR_SENSOR_PIN = 23
+GPIO.setup(ROOM_SENSOR_PIN, GPIO.IN)
+GPIO.setup(DOOR_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def readingRoomSensor():
+    if GPIO.input(ROOM_SENSOR_PIN):
+        print 'motion detected'
+        return 1
+    else:
+        return 0
+
+def readingDoorSensor():
+    if GPIO.input(DOOR_SENSOR_PIN):
+        print 'door opened'
+        return 1
+    else:
+        return 0
+
+def runController():
+    roomState = readingRoomSensor()
+    if roomState == 1:
+        setRoomState('yes')
+    else:
+        setRoomState('no')
+    doorState = readingDoorSensor()
+    if doorState == 1:
+        setDoorState('open')
+    else:
+        setDoorState('closed')
+
+def setRoomState(val):
+    values = {'name': val}
+    r = requests.put('http://127.0.0.1:8000/room/1/', data=values)
+    
+def setDoorState(val):
+    values = {'name': val}
+    r = requests.put('http://127.0.0.1:8000/door/1/', data=values)
+
+while True:
+    try:
+        runController()
+        time.sleep(3)
+except KeyboardInterrupt:
+        GPIO.cleanup()
+        exit()
