c925e4d0cc329af06deeb0fb9a3089351a43a0cf
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-02-15 20:11:35 -0500

    Create serializers.py

---
+++ lesson4/sensing/serializers.py
@@ -0,0 +1,14 @@
+# Serializers for room and door REST services - serializers.py
+
+from myapp.models import Room, Door
+from rest_framework import serializers
+
+class RoomSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('url', 'name')
+
+class DoorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Door
+        fields = ('url', 'name')
