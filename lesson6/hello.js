683902c02a85136d83e9475ba10673b0d515240a
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-03-27 20:49:03 -0400

    Update hello.js

--- lesson6/hello.js
+++ lesson6/hello.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+var http = require('http');
+
+http.createServer(function (request, response) {
+                  request.on('end', function () {
+                             console.log('request end event fired');
+                             });
+                  response.writeHead(200, {'Content-Type': 'text/plain'});
+                  response.end('Hello World!\n');
+                  console.log('response end call done');
+                  }).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/');
