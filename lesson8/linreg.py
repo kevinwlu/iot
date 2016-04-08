8fca09b586292a25b5afe16f555efe22b81f8b9e
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-04-08 19:20:11 -0400

    Create linreg.py

---
+++ lesson8/linreg.py
@@ -0,0 +1,10 @@
+from scipy import stats
+import matplotlib.pyplot as plt
+x = np.random.random(10)
+y = np.random.random(10)
+slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.plot(x,y,'ro')
+plt.plot([intercept,intercept+slope])
+plt.show()
