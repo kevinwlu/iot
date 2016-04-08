964c420345f4b2970dbaa2454095917c0b405e1d
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-04-07 22:07:26 -0400

    Added files via upload

---
+++ lesson3/time_example.py
@@ -0,0 +1,8 @@
+import time
+while True:
+    try:
+        nowtime = time.time()
+        print time.asctime(time.localtime(nowtime))
+        time.sleep(10)
+    except KeyboardInterrupt:
+        exit()
