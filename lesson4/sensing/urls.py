781723fe34b5657ade5a8c48856e316d7c3bdef4
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-08-21 23:30:45 -0400

    Update urls.py

--- lesson4/sensing/urls.py
+++ lesson4/sensing/urls.py
@@ -1,7 +1,4 @@
-# Django URL patterns for REST services and
-# home intrusion detection application - urls.py
-
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
 from myapp import views
@@ -16,6 +13,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^home/', views.home),
 ]
