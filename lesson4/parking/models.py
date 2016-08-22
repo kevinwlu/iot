8d229db52ba666bf36064d6d31c112fa2945a203
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-08-21 22:50:21 -0400

    Update models.py

--- lesson4/parking/models.py
+++ lesson4/parking/models.py
@@ -1,6 +1,8 @@
-# Django model for REST service - models.py
+from __future__ import unicode_literals
 
 from django.db import models
+
+# Create your models here.
 
 class State(models.Model):
     name = models.CharField(max_length=50)
