6866c3de229eed3b0e67bf20a68c98c6205c1099
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-08-21 20:11:11 -0400

    Update models.py

--- lesson4/stevens/models.py
+++ lesson4/stevens/models.py
@@ -1,5 +1,8 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
+# Create your models here.
 class TemperatureData(models.Model):
     timestamp = models.CharField(max_length=30)
     temperature = models.CharField(max_length=5)
