a7918aaef2e12ef8c402986d37e07eb0e17b4ebc
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-08-21 22:45:17 -0400

    Update urls.py

--- lesson4/parking/urls.py
+++ lesson4/parking/urls.py
@@ -1,6 +1,4 @@
-# Django URL patterns for REST service and smart parking application - urls.py
-
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
 from myapp import views
@@ -14,6 +12,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^home/', views.home),
 ]
