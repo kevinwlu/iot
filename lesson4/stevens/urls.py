8b623ef9c47d2548e0a5afcffbe18671621400cc
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-08-21 20:15:02 -0400

    Update urls.py

--- lesson4/stevens/urls.py
+++ lesson4/stevens/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 from myapp import views
 admin.autodiscover()
@@ -6,5 +6,5 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
