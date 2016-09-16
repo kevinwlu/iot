b45dfa4a21648f249d96fa67b6be36813254bae7
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-09-16 01:39:13 -0400

    Update date_example.py

--- lesson3/date_example.py
+++ lesson3/date_example.py
@@ -1,10 +1,10 @@
 from datetime import date
 now = date.today()
-print 'Date: ' + now.isoformat()
-print 'Date: ' + now.strftime('%m-%d-%y')
-print 'Day of Week: ' + now.strftime('%A')
-print 'Month: ' + now.strftime('%B')
-print 'Year: ' + now.strftime('%Y')
+print('Date: ' + now.isoformat())
+print('Date: ' + now.strftime('%m-%d-%y'))
+print('Day of Week: ' + now.strftime('%A'))
+print('Month: ' + now.strftime('%B'))
+print('Year: ' + now.strftime('%Y'))
 final = date(2016, 12, 12)
 timediff = final - now
 print('{:d} days to final'.format(timediff.days))
