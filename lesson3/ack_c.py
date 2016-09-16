b948f44a6200fcf25732ff43696fdc88c7e03e02
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-09-16 09:06:45 -0400

    Update ack_c.py

--- lesson3/ack_c.py
+++ lesson3/ack_c.py
@@ -5,6 +5,6 @@ ser = serial.Serial('/dev/ttyAMA0', 9600)
 while True:
 	try:
 		incoming = ser.readline().strip()
-		print 'COORDINATOR received "%s"' % incoming
+		print('COORDINATOR received "%s"' % incoming)
 	except KeyboardInterrupt:
 		exit()
