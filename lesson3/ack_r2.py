1c05278639ea7a0c8852b4469257f8edb09122ff
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-09-16 09:07:27 -0400

    Update ack_r2.py

--- lesson3/ack_r2.py
+++ lesson3/ack_r2.py
@@ -5,6 +5,6 @@ ser = serial.Serial('/dev/ttyAMA0', 9600)
 while True:
 	try:
 		incoming = ser.readline().strip()
-		print 'ROUTER_2 received "%s"' % incoming
+		print('ROUTER_2 received "%s"' % incoming)
 	except KeyboardInterrupt:
 		exit()
