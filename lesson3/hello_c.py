90a743be8ad6d4fe9fd702e496957c663f73ed28
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-09-16 09:07:49 -0400

    Update hello_c.py

--- lesson3/hello_c.py
+++ lesson3/hello_c.py
@@ -3,5 +3,5 @@ import serial
 #ser = serial.Serial('/dev/cu.usbserial-AL0046V6', 9600)
 ser = serial.Serial('/dev/ttyAMA0', 9600)
 outgoing = 'Hello from COORDINATOR'
-print 'COORDINATOR sent "%s"' % outgoing
+print('COORDINATOR sent "%s"' % outgoing)
 ser.write('%s\n' % outgoing)
