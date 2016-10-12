5392a17780487d47025cba10a482b59355cb043b
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-10-11 22:24:36 -0400

    Update settings.py

--- lesson4/stevens/settings.py
+++ lesson4/stevens/settings.py
@@ -1,3 +1,4 @@
+# Edit settings.py that was generated from the command 'django-admin startproject stevens'
 # Add 'myapp' to INSTALLED_APPS
 
 INSTALLED_APPS = [
@@ -7,17 +8,17 @@ INSTALLED_APPS = [
                   'django.contrib.sessions',
                   'django.contrib.messages',
                   'django.contrib.staticfiles',
-                  'myapp',
+                  'myapp', # add this line
 ]
 
 # Change database to MySQL
 
 DATABASES = {
-    'defalut': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stevens',
-	'USER': 'root',
-        'PASSWORD': 'password',
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # change sqlite3 to mysql
+        'NAME': 'stevens', # change os.path.join(BASE_DIR, 'db.sqlite3') to stevens
+	'USER': 'root', # mysql root user
+        'PASSWORD': 'password', # mysql root password
         'HOST': '', # set to empty for localhost
         'PORT': '', # set to empty for default port
         'OPTIONS': {
