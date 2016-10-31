5e9dcf0b1b9e05d64949cb5ffda69b606d2f4acb
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-10-30 20:39:00 -0400

    Create settings.py

---
+++ lesson4/weather/settings.py
@@ -0,0 +1,32 @@
+# Add 'myapp' and 'rest_framework' to INSTALLED_APPS
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'myapp',
+    'rest_framework',
+]
+
+# Change database to MySQL
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'weather',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+# Change TIME_ZONE
+
+TIME_ZONE = 'America/New_York'
