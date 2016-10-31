d473c3219e660d9d86f542810fab40f74d6e852a
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-10-30 20:50:29 -0400

    Create controller.py

---
+++ lesson4/weather/controller.py
@@ -0,0 +1,34 @@
+import time
+import datetime
+import Adafruit_DHT
+import sys
+import requests
+DHT_TYPE = Adafruit_DHT.DHT22
+DHT_PIN  = 24
+def runController():
+    dt = datetime.datetime.now()
+    print(dt)
+    print('Temperature: {0:0.1f} C'.format(tmp))
+    print('Humidity:    {0:0.1f} %'.format(hmd))
+    setDtState(dt)
+    setTmpState(tmp)
+    setHmdState(hmd)
+def setDtState(val):
+    values = {'name': val}
+    r = requests.put('http://127.0.0.1:8000/dt/1/', data=values, auth=('username', 'password'))
+def setTmpState(val):
+    values = {'name': val}
+    r = requests.put('http://127.0.0.1:8000/tmp/1/', data=values, auth=('username', 'password'))
+def setHmdState(val):
+    values = {'name': val}
+    r = requests.put('http://127.0.0.1:8000/hmd/1/', data=values, auth=('username', 'password'))
+while True:
+    try:
+        hmd, tmp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
+        if hmd is None or tmp is None:
+            time.sleep(2)
+            continue
+        runController()
+        time.sleep(10)
+    except KeyboardInterrupt:
+        exit()
