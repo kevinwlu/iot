dee837230712092211f28dd3ff7b83cde075c153
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-11-11 16:16:47 -0500

    Update plot_cv_predict.py

--- lesson8/plot_cv_predict.py
+++ lesson8/plot_cv_predict.py
@@ -8,13 +8,14 @@ errors.
 
 """
 from sklearn import datasets
-from sklearn.cross_validation import cross_val_predict
+from sklearn.model_selection import cross_val_predict
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 
 lr = linear_model.LinearRegression()
 boston = datasets.load_boston()
 y = boston.target
+print('Number of instances: %d' % (boston.data.shape[0]))
 
 # cross_val_predict returns an array of the same size as `y` where each entry
 # is a prediction obtained by cross validated:
