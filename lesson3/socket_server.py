1a8386bb3c3302f123d9b33d1e2bcc6df15f8f4a
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-11-29 08:32:16 -0500

    Update socket_server.py

--- lesson3/socket_server.py
+++ lesson3/socket_server.py
@@ -1,6 +1,5 @@
 # Echo server program
 import socket
-
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
