21d86b4dd0e6eadc2df8605ccd83a97fce9a23ca
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-11-29 08:32:00 -0500

    Update socket_client.py

--- lesson3/socket_client.py
+++ lesson3/socket_client.py
@@ -1,6 +1,5 @@
 # Echo client program
 import socket
-
 HOST = '192.168.1.xxx'    # The remote host IP address
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
