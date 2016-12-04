ce0d82fbce1f5be75aeab4a8c5bc3475798bee23
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-12-03 20:21:34 -0500

    Update urls.py

--- lesson4/stevens/urls.py
+++ lesson4/stevens/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from myapp import views
+
 admin.autodiscover()
 
 urlpatterns = [
+    url(r'^favicon.ico$', RedirectView.as_view(
+                          url=staticfiles_storage.url('favicon.ico'),
+                          permanent=False), name="favicon"),
     url(r'^$', views.home, name='home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
