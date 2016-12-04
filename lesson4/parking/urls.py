4e87a2e9d57fde301e3aa5c27b82d7a2718ab2b8
Kevin W. Lu <kevin.wen.lu@gmail.com>
2016-12-03 20:19:14 -0500

    Update urls.py

--- lesson4/parking/urls.py
+++ lesson4/parking/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from rest_framework import routers
 from myapp import views
 
@@ -9,6 +11,9 @@ router = routers.DefaultRouter()
 router.register(r'state', views.StateViewSet)
 
 urlpatterns = [
+    url(r'^favicon.ico$', RedirectView.as_view(
+                          url=staticfiles_storage.url('favicon.ico'),
+                          permanent=False), name="favicon"),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
