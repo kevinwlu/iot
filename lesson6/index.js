7559cf9316428403f9f7da5c95228154bc4049a2
Kevin W. Lu <kevin.wen.lu@gmail.com>
2017-01-31 10:54:40 -0500

    Create index.js

---
+++ lesson6/index.js
@@ -0,0 +1,13 @@
+// Import the interface to Tessel hardware
+var tessel = require('tessel');
+
+// Turn one of the LEDs on to start.
+tessel.led[2].on();
+
+// Blink!
+setInterval(function () {
+  tessel.led[2].toggle();
+  tessel.led[3].toggle();
+}, 100);
+
+console.log("I'm blinking! (Press CTRL + C to stop)");
