949e47be5c7a58950cbe08964171e966b696a721
Kevin W. Lu <kevin.wen.lu@gmail.com>
2017-02-19 00:35:20 -0500

    Update tmp36.py

--- lesson7/tmp36.py
+++ lesson7/tmp36.py
@@ -3,6 +3,7 @@ import json
 import time
 import readadc
 import datetime
+import RPi.GPIO as GPIO
 
 with open('./config.json') as config_file:
     plotly_user_config = json.load(config_file)
@@ -29,28 +30,29 @@ stream.open()
 
 #the main sensor reading and plotting loop
 while True:
-  try:
-    sensor_data = readadc.readadc(sensor_pin,
-                                  readadc.PINS.SPICLK,
-                                  readadc.PINS.SPIMOSI,
-                                  readadc.PINS.SPIMISO,
-                                  readadc.PINS.SPICS)
+    try:
+        sensor_data = readadc.readadc(sensor_pin,
+                                      readadc.PINS.SPICLK,
+                                      readadc.PINS.SPIMOSI,
+                                      readadc.PINS.SPIMISO,
+                                      readadc.PINS.SPICS)
 
-    millivolts = sensor_data * (3300.0 / 1024.0)
-    # 10 mv per degree
-    temp_C = ((millivolts - 100.0) / 10.0) - 40.0
-    # convert celsius to fahrenheit
-    temp_F = (temp_C * 9.0 / 5.0) + 32
-    # remove decimal point from millivolts
-    millivolts = "%d" % millivolts
-    # show only one decimal place for temprature and voltage readings
-    temp_C = "%.1f" % temp_C
-    temp_F = "%.1f" % temp_F
+        millivolts = sensor_data * (3300.0 / 1024.0)
+        # 10 mv per degree
+        temp_C = ((millivolts - 100.0) / 10.0) - 40.0
+        # convert celsius to fahrenheit
+        temp_F = (temp_C * 9.0 / 5.0) + 32
+        # remove decimal point from millivolts
+        millivolts = "%d" % millivolts
+        # show only one decimal place for temprature and voltage readings
+        temp_C = "%.1f" % temp_C
+        temp_F = "%.1f" % temp_F
 
-    # write the data to plotly
-    stream.write({'x': datetime.datetime.now(), 'y': temp_C})
+        # write the data to plotly
+        stream.write({'x': datetime.datetime.now(), 'y': temp_C})
 
-    # delay between stream posts
-    time.sleep(0.25)
-  except KeyboardInterrupt:
-    exit()
+        # delay between stream posts
+        time.sleep(0.25)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        exit()
