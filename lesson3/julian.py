dd1e6e17fd0735eabc1cc55f9f1f36bc4de59726
Kevin W. Lu <kevin.wen.lu@gmail.com>
2017-05-10 07:40:56 -0400

    Update julian.py

--- lesson3/julian.py
+++ lesson3/julian.py
@@ -3,6 +3,6 @@ from jdcal import gcal2jd, jd2gcal
 now = date.today()
 jd = gcal2jd(now.year, now.month, now.day)
 gcal = jd2gcal(jd[0], jd[1])
+print('Calendar Date: {:s}'.format(gcal))
 print('Julian Date: {:0.1f}'.format(jd[0]+jd[1]))
 print('Modified Julian Date: {:s}'.format(jd))
-print('Calendar Date: {:s}'.format(gcal))
