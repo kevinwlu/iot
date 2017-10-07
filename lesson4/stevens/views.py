46b295f4d7a8fd057d3730e14ad104f042104883
Kevin W. Lu <kevin.wen.lu@gmail.com>
2017-10-07 16:50:44 -0400

    Update views.py

--- lesson4/stevens/views.py
+++ lesson4/stevens/views.py
@@ -8,10 +8,10 @@ from django.template import RequestContext
 # Create your views here.
 def home(request):
     tempData = TemperatureData.objects.order_by('-id')[0]
-    timestamp = tempData.timestamp
+    date_time = tempData.date_time
     temperature = tempData.temperature
-    lat = tempData.lat
-    lon = tempData.lon
+    lat = tempData.latitude
+    lon = tempData.longitude
 
-    return render(request, 'myapp/index.html', {'timestamp': timestamp,
+    return render(request, 'myapp/index.html', {'date_time': date_time,
     'temperature': temperature, 'lat': lat, 'lon': lon})
