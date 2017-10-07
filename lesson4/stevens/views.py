3165af491923d07be706ef6e95f79f7e8f8899dc
Kevin W. Lu <kevin.wen.lu@gmail.com>
2017-10-06 22:21:22 -0400

    Update views.py

--- lesson4/stevens/views.py
+++ lesson4/stevens/views.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.shortcuts import render
 from myapp.models import TemperatureData
 from django.template import RequestContext
