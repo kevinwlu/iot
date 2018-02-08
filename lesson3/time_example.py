34e150c0226ead28de6e9881a5fd3697a68c1849
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-08 11:39:49 -0500

    Update time_example.py

--- lesson3/time_example.py
+++ lesson3/time_example.py
@@ -2,7 +2,7 @@ import time
 while True:
     try:
         nowtime = time.time()
-        print time.asctime(time.localtime(nowtime))
+        print(time.asctime(time.localtime(nowtime)))
         time.sleep(10)
     except KeyboardInterrupt:
         exit()
