5f14a2b7a5553e5a5cd8abf2c36856c96e7d5603
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-09 16:24:04 -0500

    Update socket_client.py

--- lesson3/socket_client.py
+++ lesson3/socket_client.py
@@ -5,7 +5,7 @@ HOST = sys.argv[1]        # The remote host IP address
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
+s.sendall('Hello, world'.encode())
 data = s.recv(1024)
 s.close()
 print('Received', repr(data))
