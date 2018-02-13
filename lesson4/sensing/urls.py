8190104812863f5d2e687831c85a120a0c611bd5
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-13 11:10:17 -0500

    Update urls.py

--- lesson4/sensing/urls.py
+++ lesson4/sensing/urls.py
@@ -1,4 +1,19 @@
-from django.conf.urls import url, include
+"""sensing URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
@@ -8,16 +23,16 @@ from myapp import views
 admin.autodiscover()
 
 router = routers.DefaultRouter()
-router.register(r'room', views.RoomViewSet)
-router.register(r'door', views.DoorViewSet)
+router.register('room', views.RoomViewSet)
+router.register('door', views.DoorViewSet)
 
 urlpatterns = [
-    url(r'^favicon.ico$', RedirectView.as_view(
+    path('favicon.ico', RedirectView.as_view(
                           url=staticfiles_storage.url('favicon.ico'),
                           permanent=False), name="favicon"),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls',
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^home/', views.home),
+    path('admin/', admin.site.urls),
+    path('home/', views.home),
 ]
