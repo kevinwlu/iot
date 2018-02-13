8750adc6385ebf0c8ded04cbd5371788caf2f977
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-13 11:02:24 -0500

    Update urls.py

--- lesson4/stevens/urls.py
+++ lesson4/stevens/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 from myapp import views
