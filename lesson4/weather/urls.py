893d737107f2ee097c260af0c15d3850d1a5996c
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-13 10:50:21 -0500

    Update urls.py

--- lesson4/weather/urls.py
+++ lesson4/weather/urls.py
@@ -1,20 +1,20 @@
 """weather URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import include, path
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 from rest_framework import routers
@@ -23,17 +23,17 @@ from myapp import views
 admin.autodiscover()
 
 router = routers.DefaultRouter()
-router.register(r'dt', views.DtViewSet)
-router.register(r'tmp', views.TmpViewSet)
-router.register(r'hmd', views.HmdViewSet)
+router.register('dt', views.DtViewSet)
+router.register('tmp', views.TmpViewSet)
+router.register('hmd', views.HmdViewSet)
 
 urlpatterns = [
-    url(r'^favicon.ico$', RedirectView.as_view(
-                          url=staticfiles_storage.url('favicon.ico'),
-                          permanent=False), name="favicon"),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls',
+    path('favicon.ico', RedirectView.as_view(
+                        url=staticfiles_storage.url('favicon.ico'),
+                        permanent=False), name="favicon"),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^home/', views.home),
+    path('admin/', admin.site.urls),
+    path('home/', views.home),
 ]
