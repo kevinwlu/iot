7cf3013ffe134c19fc4d12c679ff53bff3d7470c
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-13 10:50:48 -0500

    Update urls.py

--- lesson4/stevens/urls.py
+++ lesson4/stevens/urls.py
@@ -23,8 +23,8 @@ admin.autodiscover()
 
 urlpatterns = [
     path('favicon.ico', RedirectView.as_view(
-                          url=staticfiles_storage.url('favicon.ico'),
-                          permanent=False), name="favicon"),
+                        url=staticfiles_storage.url('favicon.ico'),
+                        permanent=False), name="favicon"),
     path('', views.home, name='home'),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
