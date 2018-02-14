23bbd822c7a555ba831aeaea85f9dbd9472cd117
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-14 14:23:54 -0500

    Update urls.py

--- lesson4/parking/urls.py
+++ lesson4/parking/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 from rest_framework import routers
