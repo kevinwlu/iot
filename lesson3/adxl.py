347f5c2721e53832e9a5b8c587f8527e82fa3030
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-14 23:04:49 -0500

    Update adxl.py

--- lesson3/adxl.py
+++ lesson3/adxl.py
@@ -2,11 +2,11 @@ import time
 from adxl345 import ADXL345
 
 adxl345 = ADXL345()
-print "ADXL345 on address 0x%x:" % (adxl345.address)
+print("ADXL345 on address 0x%x:" % (adxl345.address))
 
 def runController():
     axes = adxl345.getAxes(True)
-    print 'x = %.3f y = %.3f z = %.3f' % (axes['x'], axes['y'], axes['z'])
+    print('x = %.3f y = %.3f z = %.3f' % (axes['x'], axes['y'], axes['z']))
 
 while True:
     runController()
