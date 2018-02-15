f61d9bb6baa89ddfcc1b063c8ef2dc1e3796b0ca
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-14 22:55:26 -0500

    Create spi.py

---
+++ lesson3/spi.py
@@ -0,0 +1,20 @@
+import sys
+import spidev
+channel = 0
+spi = spidev.SpiDev()
+spi.open(0, 0)
+adc = spi.xfer2([1, (8 + channel) << 4, 0])
+print('adc = ', adc)
+data = ((adc[1]&3) << 8) + adc[2]
+print('data = ', data)
+R = 10
+places = 2
+volts = (data * 3.3) / 1023
+volts = round(volts, places)
+print('volts = ', volts)
+if volts == 0:
+    lux = 0
+else:
+    lux = 500 * (3.3 - volts) / (R * volts)
+print('lux = ', lux)
+spi.close()
