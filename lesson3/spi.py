dec91d0e86947bf924e14162da6a9ae62c491d76
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-16 22:04:08 -0500

    Update spi.py

--- lesson3/spi.py
+++ lesson3/spi.py
@@ -3,6 +3,7 @@ import spidev
 channel = 0
 spi = spidev.SpiDev()
 spi.open(0, 0)
+spi.max_speed_hz = 976000
 adc = spi.xfer2([1, (8 + channel) << 4, 0])
 print('adc = ', adc)
 data = ((adc[1]&3) << 8) + adc[2]
