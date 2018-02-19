0452a7d7b5acd78ccf9b282f2fb15e9681b03ae1
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-19 09:09:52 -0500

    Update thingspeak_cpu_loop.py

--- lesson7/thingspeak_cpu_loop.py
+++ lesson7/thingspeak_cpu_loop.py
@@ -14,14 +14,14 @@ def doit():
 	try:
 		conn.request("POST", "/update", params, headers)
 		response = conn.getresponse()
-		print cpu_pc
-		print mem_avail_mb
-		print strftime("%a, %d %b %Y %H:%M:%S", localtime())
-		print response.status, response.reason
+		print(cpu_pc)
+		print(mem_avail_mb)
+		print(strftime("%a, %d %b %Y %H:%M:%S", localtime()))
+		print(response.status, response.reason)
 		data = response.read()
 		conn.close()
 	except:
-		print "connection failed"	
+		print("connection failed")	
 #sleep for 60 seconds (api limit of 15 secs)
 if __name__ == "__main__":
 	while True:
