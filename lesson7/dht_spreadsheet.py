13ffdc6c316c2df3fff81ee932c03fdd6e79bb64
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-19 09:06:27 -0500

    Update dht_spreadsheet.py

--- lesson7/dht_spreadsheet.py
+++ lesson7/dht_spreadsheet.py
@@ -94,12 +94,12 @@ def login_open_sheet(oauth_key_file, spreadsheet):
 		worksheet = gc.open(spreadsheet).sheet1
 		return worksheet
 	except Exception as ex:
-		print 'Unable to login and get spreadsheet.  Check OAuth credentials, spreadsheet name, and make sure spreadsheet is shared to the client_email address in the OAuth .json file!'
-		print 'Google sheet login failed with error:', ex
+		print('Unable to login and get spreadsheet.  Check OAuth credentials, spreadsheet name, and make sure spreadsheet is shared to the client_email address in the OAuth .json file!')
+		print('Google sheet login failed with error:', ex)
 		sys.exit(1)
 
-print 'Logging sensor measurements to {0} every {1} seconds.'.format(GDOCS_SPREADSHEET_NAME, FREQUENCY_SECONDS)
-print 'Press Ctrl-C to quit.'
+print('Logging sensor measurements to {0} every {1} seconds.'.format(GDOCS_SPREADSHEET_NAME, FREQUENCY_SECONDS))
+print('Press Ctrl-C to quit.')
 worksheet = None
 while True:
 	# Login if necessary.
@@ -116,8 +116,8 @@ while True:
 		time.sleep(2)
 		continue
 
-	print 'Temperature: {0:0.1f} C'.format(temp)
-	print 'Humidity:    {0:0.1f} %'.format(humidity)
+	print('Temperature: {0:0.1f} C'.format(temp))
+	print('Humidity:    {0:0.1f} %'.format(humidity))
  
 	# Append the data in the spreadsheet, including a timestamp
 	try:
@@ -125,11 +125,11 @@ while True:
 	except:
 		# Error appending data, most likely because credentials are stale.
 		# Null out the worksheet so a login is performed at the top of the loop.
-		print 'Append error, logging in again'
+		print('Append error, logging in again')
 		worksheet = None
 		time.sleep(FREQUENCY_SECONDS)
 		continue
 
 	# Wait 30 seconds before continuing
-	print 'Wrote a row to {0}'.format(GDOCS_SPREADSHEET_NAME)
+	print('Wrote a row to {0}'.format(GDOCS_SPREADSHEET_NAME))
 	time.sleep(FREQUENCY_SECONDS)
