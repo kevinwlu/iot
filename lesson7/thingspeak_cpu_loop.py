f272e6202ab519a278f738f5fb919d7097f9df65
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-19 16:22:40 -0500

    Update thingspeak_cpu_loop.py

--- lesson7/thingspeak_cpu_loop.py
+++ lesson7/thingspeak_cpu_loop.py
@@ -1,4 +1,4 @@
-import httplib, urllib
+import http.client, urllib.request, urllib.parse, urllib.error
 from time import localtime, strftime
 import psutil
 import time
@@ -6,11 +6,11 @@ def doit():
 	cpu_pc = psutil.cpu_percent()
 	mem = psutil.virtual_memory()
 	mem_avail_mb = mem.available/(1024*1024)
-	params = urllib.urlencode({'field1': cpu_pc, 'field2':         
+	params = urllib.parse.urlencode({'field1': cpu_pc, 'field2':         
              mem_avail_mb,'key':'YOURKEYHERE'})
 	headers = {"Content-type": 
                "application/x-www-form-urlencoded","Accept": "text/plain"}
-	conn = httplib.HTTPConnection("api.thingspeak.com:80")
+	conn = http.client.HTTPConnection("api.thingspeak.com:80")
 	try:
 		conn.request("POST", "/update", params, headers)
 		response = conn.getresponse()
