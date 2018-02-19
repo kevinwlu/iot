c64a4bfabb14194d530d166f773fcbe61708e204
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-19 09:08:51 -0500

    Update rpi_spreadsheet.py

--- lesson7/rpi_spreadsheet.py
+++ lesson7/rpi_spreadsheet.py
@@ -18,11 +18,11 @@ def login_open_sheet(oauth_key_file, spreadsheet):
 		worksheet = gc.open(spreadsheet).sheet1
 		return worksheet
 	except Exception as ex:
-		print 'Unable to login and get spreadsheet.  Check OAuth credentials, spreadsheet name, and make sure spreadsheet is shared to the client_email address in the OAuth .json file!'
-		print 'Google sheet login failed with error:', ex
+		print('Unable to login and get spreadsheet.  Check OAuth credentials, spreadsheet name, and make sure spreadsheet is shared to the client_email address in the OAuth .json file!')
+		print('Google sheet login failed with error:', ex)
 		sys.exit(1)
-print 'Logging sensor measurements to {0} every {1} seconds.'.format(GDOCS_SPREADSHEET_NAME, FREQUENCY_SECONDS)
-print 'Press Ctrl-C to quit.'
+print('Logging sensor measurements to {0} every {1} seconds.'.format(GDOCS_SPREADSHEET_NAME, FREQUENCY_SECONDS))
+print('Press Ctrl-C to quit.')
 worksheet = None
 while True:
 	if worksheet is None:
@@ -36,9 +36,9 @@ while True:
 	try:
 		worksheet.append_row((dat, cpu, tmp))
 	except:
-		print 'Append error, logging in again'
+		print('Append error, logging in again')
 		worksheet = None
 		time.sleep(FREQUENCY_SECONDS)
 		continue
-	print 'Wrote a row to {0}'.format(GDOCS_SPREADSHEET_NAME)
+	print('Wrote a row to {0}'.format(GDOCS_SPREADSHEET_NAME))
 	time.sleep(FREQUENCY_SECONDS)
