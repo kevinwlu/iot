6ad92e485c11ee5b093b1929ce9776ad4570444a
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-02-24 12:44:07 -0500

    Update sub-multiple.py

--- lesson5/sub-multiple.py
+++ lesson5/sub-multiple.py
@@ -3,7 +3,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("paho/test/multiple")
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic+" "+str(msg.payload.decode()))
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
