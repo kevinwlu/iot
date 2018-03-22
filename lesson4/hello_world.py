6f1793ff308054a9fb276c0ccebda82c385cd425
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-03-22 13:14:43 -0400

    Update hello_world.py

--- lesson4/hello_world.py
+++ lesson4/hello_world.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask import send_from_directory
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', 
+                               mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run()
