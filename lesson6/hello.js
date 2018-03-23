566c9811a5d9d28046c3252f009c953ff809b9c4
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-03-23 09:28:43 -0400

    Update hello.js

--- lesson6/hello.js
+++ lesson6/hello.js
@@ -1,11 +1,21 @@
 var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+var favicon = path.join(__dirname, 'static', 'favicon.ico');
 
-http.createServer(function (request, response) {
-                  request.on('end', function () {
-                             console.log('request end event fired');
-                             });
-                  response.writeHead(200, {'Content-Type': 'text/plain'});
-                  response.end('Hello World!\n');
+http.createServer(function (req, res) {
+                  var pathname = url.parse(req.url).pathname;
+                  if (req.method === 'GET' && pathname === '/favicon.ico') {
+                    res.setHeader('Content-Type', 'image/x-icon');
+                    fs.createReadStream(favicon).pipe(res);
+                    return;
+                  }
+                  req.on('end', function () {
+                         console.log('request end event fired');
+                         });
+                  res.writeHead(200, {'Content-Type': 'text/plain'});
+                  res.end('Hello World!\n');
                   console.log('response end call done');
                   }).listen(8080);
 
