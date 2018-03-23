a1ab981e0cf8f3c1dc3c6ce112aa29247e2fd202
Kevin W. Lu <kevin.wen.lu@gmail.com>
2018-03-23 10:02:35 -0400

    Update http.js

--- lesson6/http.js
+++ lesson6/http.js
@@ -1,18 +1,28 @@
-var http = require("http"),
-fs = require("fs");
-http.createServer(function (request, response) {
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+var favicon = path.join(__dirname, 'static', 'favicon.ico');
+
+http.createServer(function (req, res) {
+                  var pathname = url.parse(req.url).pathname;
+                  if (req.method === 'GET' && pathname === '/favicon.ico') {
+                    res.setHeader('Content-Type', 'image/x-icon');
+                    fs.createReadStream(favicon).pipe(res);
+                    return;
+                  }
                   fs.readFile("test.txt", 'utf-8', function (error, data) {
                               console.log(data);
-                              response.writeHead(200, {
-                                                 'Content-Type': 'text/plain'
-                                                 });
+                              res.writeHead(200, {
+                                            'Content-Type': 'text/plain'
+                                            });
                               if(!data){
                               var data = 0;
                               }
                               data = (parseInt(data) + 1);
                               var refresh = parseInt(data);
                               fs.writeFile('test.txt', parseInt(refresh));
-                              response.end('This page was refreshed ' + data + ' times!');
+                              res.end('This page was refreshed ' + data + ' times!');
                               var data = "";
                               });
                   }).listen(8080);
