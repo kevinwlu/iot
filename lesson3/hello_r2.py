fc06ab7e5a2055bb849ce1e182581faf41f7820e
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2018-09-24 12:24:59 -0400

    Update hello_r2.py

--- lesson3/hello_r2.py
+++ lesson3/hello_r2.py
@@ -1,6 +1,6 @@
 import serial
 
 ser = serial.Serial('/dev/ttyAMA0', 9600)
-outgoing = 'Hello from ROUTER 2'
-print('Sent "%s"' % outgoing)
+outgoing = b'Hello from ROUTER 2'
+print('Sent "%s"' % outgoing.decode())
 ser.write('%s\n' % outgoing)
