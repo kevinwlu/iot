dac0ab4aaef82a21b96e81f6ebee8e9d4efb5c52
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2018-09-26 13:26:01 -0400

    Update socket_client.py

--- lesson3/socket_client.py
+++ lesson3/socket_client.py
@@ -6,6 +6,6 @@ PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 s.sendall('Hello, world'.encode())
-data = s.recv(1024)
+data = s.recv(1024).decode()
 s.close()
 print('Received', repr(data))
