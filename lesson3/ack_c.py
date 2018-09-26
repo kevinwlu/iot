2c37864dc3a750cd147c5154fcb52c115bf30fa2
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2018-09-26 09:38:09 -0400

    Update ack_c.py

--- lesson3/ack_c.py
+++ lesson3/ack_c.py
@@ -2,6 +2,7 @@ import serial
 
 #ser = serial.Serial('/dev/cu.usbserial-AL0046V6', 9600)
 ser = serial.Serial('/dev/ttyAMA0', 9600)
+#ser = serial.Serial('/dev/ttyS0', 9600)
 while True:
 	try:
 		incoming = ser.readline().strip()
