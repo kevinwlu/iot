d93ab21651aa5124b28806dd56cb3d469bfefca7
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2018-09-26 20:18:44 -0400

    Update controller.py

--- lesson4/sensing/controller.py
+++ lesson4/sensing/controller.py
@@ -37,11 +37,11 @@ def runController():
 
 def setRoomState(val):
     values = {'name': val}
-    r = requests.put('http://127.0.0.1:8000/room/1/', data=values, auth=('pi', 'PASSWORD'))
+    r = requests.put('http://127.0.0.1:8000/room/1/', data=values, auth=('pi', 'raspberry'))
 
 def setDoorState(val):
     values = {'name': val}
-    r = requests.put('http://127.0.0.1:8000/door/1/', data=values, auth=('pi', 'PASSWORD'))
+    r = requests.put('http://127.0.0.1:8000/door/1/', data=values, auth=('pi', 'raspberry'))
 
 while True:
     try:
