afeac3d4d4cf64adae1203064286b7a46bcaa5b1
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2018-10-14 20:21:56 -0400

    Update client.py

--- lesson5/client.py
+++ lesson5/client.py
@@ -1,12 +1,11 @@
 import paho.mqtt.client as mqtt
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
-
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/broker/publish/#")
+    client.subscribe("$SYS/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
