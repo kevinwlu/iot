94ca3108c64682c84c0879455f77e7678270d236
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2018-11-10 23:17:36 -0500

    Update keras_diabetes.py

--- lesson8/keras_diabetes.py
+++ lesson8/keras_diabetes.py
@@ -6,10 +6,7 @@ import numpy
 # fix random seed for reproducibility
 numpy.random.seed(7)
 # load pima indians dataset
-# dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
-filename = 'pima-indians-diabetes.data.csv'
-raw_data = open(filename, 'rt')
-data = numpy.loadtxt(raw_data, delimiter=",")
+dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:8]
 Y = dataset[:,8]
