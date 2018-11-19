ec9c43719cd3688aa69eccd4bcb4b66c5c47421b
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2018-11-18 20:52:30 -0500

    Update my_ip.py

--- lesson1/my_ip.py
+++ lesson1/my_ip.py
@@ -1,27 +1,19 @@
 import socket
 import smtplib
 from email.mime.text import MIMEText
-
 to = 'RECIPIENT_EMAIL' # Email to send to
 gmail_user = 'GMAIL_USERNAME' # Email to send from (MUST BE GMAIL)
-gmail_password = 'GOOGLE_APP_PASSWORD' # 16-digit Google App Password required for 2-Step Verification
-smtpserver = smtplib.SMTP('smtp.gmail.com', 587) # Server to use
-smtpserver.ehlo()  # Says 'hello' to the server
-smtpserver.starttls()  # Start TLS encryption
+gmail_password = 'GOOGLE_APP_PASSWORD' # Enable 2-Step Verification and 16-digit Google App Password
+smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
 smtpserver.ehlo()
-smtpserver.login(gmail_user, gmail_password)  # Log in to server
-
+smtpserver.starttls()
+smtpserver.ehlo()
+smtpserver.login(gmail_user, gmail_password)
 ipaddr = socket.gethostbyname(socket.gethostname())
-
-# Creates a sentence for each IP address
-my_ip = 'RaspberryPi IP address is %s' % ipaddr
-
-# Creates the text, subject, 'from', and 'to' of the message
+my_ip = 'HOSTNAME IP address is %s' % ipaddr # Change HOSTNAME
 msg = MIMEText(my_ip)
-msg['Subject'] = 'IP address for RaspberryPi'
+msg['Subject'] = 'IP address for HOSTNAME' # Change HOSTNAME
 msg['From'] = gmail_user
 msg['To'] = to
-# Sends the message
 smtpserver.sendmail(gmail_user, [to], msg.as_string())
-# Closes the smtp server
 smtpserver.quit()
