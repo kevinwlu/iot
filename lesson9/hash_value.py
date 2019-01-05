419c5e12ecd60122f6852830d45a401abe292bc2
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-01-04 20:00:44 -0500

    Create hash_value.py

---
+++ lesson9/hash_value.py
@@ -0,0 +1,25 @@
+# hash for integer unchanged
+print('Hash for 181 is:', hash(181))
+
+# hash for decimal
+print('Hash for 181.23 is:',hash(181.23))
+
+# hash for string
+print('Hash for Python is:', hash('Python'))
+
+# hash for tuple of vowels
+vowels = ('a', 'e', 'i', 'o', 'u')
+print('The hash is:', hash(vowels))
+
+# hash for custom object
+class Person:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+    def __eq__(self, other):
+        return self.age == other.age and self.name == other.name
+    def __hash__(self):
+        print('The hash is:')
+        return hash((self.age, self.name))
+person = Person(23, 'Adam')
+print(hash(person))
