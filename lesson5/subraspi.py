3e11877ff01d7a450a5ba05b2f93c450364040df
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-01-05 22:57:33 -0500

    Create subraspi.py

---
+++ lesson5/subraspi.py
@@ -0,0 +1,11 @@
+import paho.mqtt.client as mqtt
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("Raspberry Pi")
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload.decode()))
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("iot.eclipse.org", 1883, 60)
+client.loop_forever()
