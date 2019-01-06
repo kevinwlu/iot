f4dd1bc2dea911016793951e8635d8ddb0341895
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-01-06 00:46:04 -0500

    Create models.py

---
+++ lesson4/myraspi/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class LocationData(models.Model):
+    location = models.CharField(max_length=30)
+    latitude = models.CharField(max_length=20)
+    longitude = models.CharField(max_length=20)
+    def __str__(self):
+        return self.location
+
+class Dt(models.Model):
+    name = models.CharField(max_length=30)
+
+class Tmp(models.Model):
+    name = models.CharField(max_length=50)
+
+class Cpu(models.Model):
+    name = models.CharField(max_length=50)
