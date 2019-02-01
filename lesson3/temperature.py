6112671ab5a138d1f3652d735c0a4d402d90a24b
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-02-01 11:29:45 -0500

    Update and rename 3-temperature.py to temperature.py

--- lesson3/temperature.py
+++ lesson3/temperature.py
@@ -15,34 +15,37 @@ device_file = device_folder + '/w1_slave'
  
 # A function that reads the sensors data
 def read_temp_raw():
-  f = open(device_file, 'r') # Opens the temperature device file
-  lines = f.readlines() # Returns the text
-  f.close()
-  return lines
+    f = open(device_file, 'r') # Opens the temperature device file
+    lines = f.readlines() # Returns the text
+    f.close()
+    return lines
  
 # Convert the value of the sensor into a temperature
 def read_temp():
-  lines = read_temp_raw() # Read the temperature 'device file'
+    lines = read_temp_raw() # Read the temperature 'device file'
  
   # While the first line does not contain 'YES', wait for 0.2s
   # and then read the device file again.
-  while lines[0].strip()[-3:] != 'YES':
-    time.sleep(0.2)
-    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
  
   # Look for the position of the '=' in the second line of the
   # device file.
-  equals_pos = lines[1].find('t=')
+    equals_pos = lines[1].find('t=')
  
   # If the '=' is found, convert the rest of the line after the
   # '=' into degrees Celsius, then degrees Fahrenheit
-  if equals_pos != -1:
-    temp_string = lines[1][equals_pos+2:]
-    temp_c = float(temp_string) / 1000.0
-    temp_f = temp_c * 9.0 / 5.0 + 32.0
-    return temp_c, temp_f
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
  
 # Print out the temperature until the program is stopped.
 while True:
-  print(read_temp())
-  time.sleep(1)
+    try:
+        print(read_temp())
+        time.sleep(1)
+    except KeyboardInterrupt:
+        exit()
