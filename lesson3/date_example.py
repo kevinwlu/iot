fe864d385450366139f11e398cab70a4497bfa4b
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-02-16 19:45:52 -0500

    Update date_example.py

--- lesson3/date_example.py
+++ lesson3/date_example.py
@@ -5,8 +5,8 @@ print('Date: ' + now.strftime('%m-%d-%y'))
 print('Day of Week: ' + now.strftime('%A'))
 print('Month: ' + now.strftime('%B'))
 print('Year: ' + now.strftime('%Y'))
-midterm = date(2018, 10, 15)
-final = date(2018, 12, 10)
+midterm = date(2019, 03, 11)
+final = date(2019, 05, 06)
 timediff = midterm - now
 print('{:d} days to Midterm Exam'.format(timediff.days))
 timediff = final - now
