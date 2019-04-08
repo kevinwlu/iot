3920b914315000377167501adf697e416d2b3dc6
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-04-08 09:25:20 -0400

    Update thingspeak_cpu_loop.py

--- lesson7/thingspeak_cpu_loop.py
+++ lesson7/thingspeak_cpu_loop.py
@@ -6,9 +6,9 @@ def doit():
 	cpu_pc = psutil.cpu_percent()
 	mem = psutil.virtual_memory()
 	mem_avail_mb = mem.available/(1024*1024)
-	params = urllib.parse.urlencode({'field1': cpu_pc, 'field2':         
+	params = urllib.parse.urlencode({'field1': cpu_pc, 'field2':
              mem_avail_mb,'key':'YOURKEYHERE'})
-	headers = {"Content-type": 
+	headers = {"Content-type":
                "application/x-www-form-urlencoded","Accept": "text/plain"}
 	conn = http.client.HTTPConnection("api.thingspeak.com:80")
 	try:
