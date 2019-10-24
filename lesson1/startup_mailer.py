a3c00054e0cd7dd49c9b0b79f8dc927e98332036
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-10-24 10:26:21 -0400

    Update startup_mailer.py

--- lesson1/startup_mailer.py
+++ lesson1/startup_mailer.py
@@ -25,12 +25,9 @@ def connect_type(word_list):
 # Account Information
 to = 'RECIPIENT_EMAIL' # Email to send to
 gmail_user = 'GMAIL_USERNAME' # Email to send from (MUST BE GMAIL)
-gmail_password = 'GOOGLE_APP_PASSWORD' # 16-digit Google App Password if using 2-Step Verification
-# that needs a 6-digit verification code to sign in Google Account
-# https://www.google.com/landing/2step/help.html 
-# If not using 2-Step Verification, it's required to turn on access for "less secure apps"
-# https://www.google.com/settings/security/lesssecureapps
-# My Account > Sign-in & security > Connected apps & sites > Allow less secure apps: ON
+gmail_password = 'GOOGLE_APP_PASSWORD' # 16-digit Google App Password (without space)
+# from Google 2-Step Verification https://www.google.com/landing/2step/help.html
+
 smtpserver = smtplib.SMTP('smtp.gmail.com', 587) # Server to use
 
 smtpserver.ehlo()  # Says 'hello' to the server
