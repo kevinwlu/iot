45bd984b12f5f6deeec3580b6e0310fbe612a8f1
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-10-26 08:35:18 -0400

    Update histogram_demo_features.py

--- lesson8/histogram_demo_features.py
+++ lesson8/histogram_demo_features.py
@@ -7,7 +7,7 @@ sigma = 15  # standard deviation of distribution
 x = mu + sigma * np.random.randn(10000)
 num_bins = 50
 # the histogram of the data
-n, bins, patches = plt.hist(x, num_bins, normed=1, facecolor='green', alpha=0.5)
+n, bins, patches = plt.hist(x, num_bins, density=1, facecolor='green', alpha=0.5)
 # add a 'best fit' line
 y = mlab.normpdf(bins, mu, sigma)
 plt.plot(bins, y, 'r--')
