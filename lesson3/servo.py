c1dbd868003eea95b446579b3f6f06ed6e8f3c97
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-11-23 17:37:00 -0500

    Create servo.py

---
+++ lesson3/servo.py
@@ -0,0 +1,28 @@
+# https://learn.adafruit.com/adafruits-raspberry-pi-lesson-8-using-a-servo-motor/overview
+# sudo pip3 install wiringpi
+# Servo Control
+import time
+import wiringpi
+ 
+# use 'GPIO naming'
+wiringpi.wiringPiSetupGpio()
+ 
+# set #18 to be a PWM output
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+ 
+# set the PWM mode to milliseconds stype
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+ 
+# divide down clock
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+ 
+delay_period = 0.01
+ 
+while True:
+        for pulse in range(50, 250, 1):
+                wiringpi.pwmWrite(18, pulse)
+                time.sleep(delay_period)
+        for pulse in range(250, 50, -1):
+                wiringpi.pwmWrite(18, pulse)
+                time.sleep(delay_period)
