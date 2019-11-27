e83af900013e8499829be315427a5b7c0008390e
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-11-27 09:27:27 -0500

    Update servomotor.py

--- lesson3/servomotor.py
+++ lesson3/servomotor.py
@@ -3,11 +3,11 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 17
+servoPIN = 22
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p = GPIO.PWM(servoPIN, 50) # GPIO 22 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
   while True:
