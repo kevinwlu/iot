8c72d13c9dfbaff983c067daa744f92d4882a9aa
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2019-12-03 09:55:04 -0500

    Update servomotor.py

--- lesson3/servomotor.py
+++ lesson3/servomotor.py
@@ -3,6 +3,8 @@
 # TowerPro SG90 http://www.towerpro.com.tw/product/sg90-7
 # Emax ES3001 https://emaxmodel.com/es3001-43g-analog-servo.html
 # Parallax Continuous Rotation Servo https://www.parallax.com/product/900-00008
+# 2019-12-03: tested with Raspberry Pi 4B, 6V power supply from a 4xAA battery holder, and the following servo motor
+# Emax ES08A II https://emaxmodel.com/es08a-ii.html
 import RPi.GPIO as GPIO
 import time
 
