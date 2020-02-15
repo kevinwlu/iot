91cd0457a154157de8ea2e6cf35222e2c6126437
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2020-02-15 15:11:55 -0500

    Update sun.py

--- lesson3/sun.py
+++ lesson3/sun.py
@@ -1,17 +1,16 @@
 import sys
 from datetime import date
-from astral import Astral
+from astral import LocationInfo
+from astral.sun import sun
 city_name = sys.argv[1]
-a = Astral()
-a.solar_depression = 'civil'
-city = a[city_name]
+city = LocationInfo[city_name]
 print('Information for %s/%s\n' % (city_name, city.region))
 timezone = city.timezone
 print('Timezone: %s' % timezone)
 print('Latitude: %.02f; Longitude: %.02f\n' % (city.latitude, city.longitude))
-sun = city.sun(date=date.today(), local=True)
-print('Dawn:    %s' % str(sun['dawn']))
-print('Sunrise: %s' % str(sun['sunrise']))
-print('Noon:    %s' % str(sun['noon']))
-print('Sunset:  %s' % str(sun['sunset']))
-print('Dusk:    %s' % str(sun['dusk']))
+s = sun(city.observer, date=date.today(), local=True)
+print('Dawn:    %s' % str(s['dawn']))
+print('Sunrise: %s' % str(s['sunrise']))
+print('Noon:    %s' % str(s['noon']))
+print('Sunset:  %s' % str(s['sunset']))
+print('Dusk:    %s' % str(s['dusk']))
