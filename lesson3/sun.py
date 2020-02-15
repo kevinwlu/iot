5eecca8552e5c6b0f45bbb47cdbb14fec41c50d6
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2020-02-15 17:13:36 -0500

    Update sun.py

--- lesson3/sun.py
+++ lesson3/sun.py
@@ -1,4 +1,4 @@
-import sys
+import sys pytz
 from datetime import date
 from astral.geocoder import database, lookup
 from astral.sun import sun
@@ -8,7 +8,7 @@ print('Information for %s/%s\n' % (city.name, city.region))
 timezone = city.timezone
 print('Timezone: %s' % timezone)
 print('Latitude: %.02f; Longitude: %.02f\n' % (city.latitude, city.longitude))
-s = sun(city.observer, date=date.today(), tzinfo=-5)
+s = sun(city.observer, date=date.today(), tzinfo=pytz.timezone(timezone))
 print('Dawn:    %s' % str(s['dawn']))
 print('Sunrise: %s' % str(s['sunrise']))
 print('Noon:    %s' % str(s['noon']))
