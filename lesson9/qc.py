8daa3bd912b865ab1fc7ce28640c2dce9d03cfbd
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2020-04-13 19:37:34 -0400

    Create qc.py

---
+++ lesson9/qc.py
@@ -0,0 +1,14 @@
+# https://en.wikipedia.org/wiki/Qiskit
+
+from qiskit import QuantumCircuit, Aer, execute
+
+qc = QuantumCircuit(2, 2)
+
+qc.h(0)
+qc.cx(0, 1)
+qc.measure([0,1], [0,1])
+
+backend = Aer.get_backend('qasm_simulator')
+job_sim = execute(qc, backend)
+sim_result = job_sim.result()
+print(sim_result.get_counts(qc))
