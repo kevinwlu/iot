7cc44eb3b714ce6bb6dbb27a11aa1de177a8944e
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2020-10-11 21:15:31 -0400

    Update readadc.py

--- lesson7/readadc.py
+++ lesson7/readadc.py
@@ -2,23 +2,23 @@ import RPi.GPIO as GPIO
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the GPIO Pins on the Raspi
 
-# MCP3008 to Raspi (PiCobbler) Pin connections
+# MCP3008 to Raspi Pin connections
 class PINS:
     SPICLK = 11
-    SPIMISO = 9
     SPIMOSI = 10
+    SPIMISO = 9
     SPICS = 8
 
  # set up the SPI interface pins
 def initialize():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
+    GPIO.setup(PINS.SPICLK, GPIO.OUT)
     GPIO.setup(PINS.SPIMOSI, GPIO.OUT)
     GPIO.setup(PINS.SPIMISO, GPIO.IN)
-    GPIO.setup(PINS.SPICLK, GPIO.OUT)
     GPIO.setup(PINS.SPICS, GPIO.OUT)
 
-# Function to read data from Analog Pin 0 from MCP3008 (don't need to edit)
+# Function to read data from Analog Pin 1 from MCP3008
 # This function will be called in our loop to get the current sensor value
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
     if ((adcnum > 7) or (adcnum < 0)):
