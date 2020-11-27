253a9e250a894471cc6919d777ea1c6a94dc818b
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2020-11-26 19:41:44 -0500

    Update thingspeak_feed.py

--- lesson7/thingspeak_feed.py
+++ lesson7/thingspeak_feed.py
@@ -2,7 +2,15 @@ import http.client, urllib.request, urllib.parse, urllib.error
 from time import localtime, strftime
 import psutil
 import time
-from pickle import dump as pickle_dump # Pridhvi Myneni
+"""
+2020-11-26: Pridhvi Myneni added lines 13-18, 25, and 43-54
+1. gitignore (https://git-scm.com/docs/gitignore) now prevents students from pushing up credentials.
+2. This program now attempts to cache the API key, but always prompts the user for their preference prior to saving credentials locally.
+3. Pickle data type used to store credentials. Pickle is a binary storage version of JSON. Please note not-human-readable does not mean secure.
+4. Removed key storage in source, which is generally a bad practice, as it leads to version control systems having your key.
+5. Key for storage in a dictionary. This could be expanded in the future if other information needs to be similarly stored.
+"""
+from pickle import dump as pickle_dump
 from pickle import load as pickle_load
 from os.path import exists as file_exists
 from os.path import isfile as is_file
@@ -13,7 +21,8 @@ def doit():
 	mem = psutil.virtual_memory()
 	mem_avail_mb = mem.available/(1024*1024)
 	params = urllib.parse.urlencode({'field1': cpu_pc, 'field2':
-             mem_avail_mb,'key':'YOURKEYHERE'})
+#             mem_avail_mb,'key':'YOURKEYHERE'})
+             mem_avail_mb,'key':KEY})
 	headers = {"Content-type":
                "application/x-www-form-urlencoded","Accept": "text/plain"}
 	conn = http.client.HTTPConnection("api.thingspeak.com:80")
@@ -30,7 +39,8 @@ def doit():
 		print("connection failed")
 #sleep for 60 seconds (api limit of 15 secs)
 if __name__ == "__main__":
-	if file_exists(PICKLE_FILE_NAME) and is_file(PICKLE_FILE_NAME): # Pridhvi Myneni
+#caching the Write Key for the ThingSpeak API
+	if file_exists(PICKLE_FILE_NAME) and is_file(PICKLE_FILE_NAME):
 		save_file = None
 		with open(PICKLE_FILE_NAME, 'rb') as f:
 			save_file = pickle_load(f)
