9e9d33519a07f1140f4b1b8255511853d257a1e8
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2020-12-05 11:56:05 -0500

    Create mq135.py

---
+++ lesson3/mq135.py
@@ -0,0 +1,27 @@
+import spidev
+channel = 1
+
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 976000
+
+# Function to read SPI data from MCP3008 chip
+def ReadChannel(channel):
+    adc = spi.xfer2([1, (8+channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+# Function to read sensor connected to MCP3008
+def readMQ():
+    level = ReadChannel(channel)
+    return level
+
+# Controller main function
+def runController():
+    level = readMQ()
+    print('level={0:0.5f}'.format(level))
+
+while True:
+    runController()
+    time.sleep(10)
