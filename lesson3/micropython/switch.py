dffcf66f1aa241263e7caf34fda0ac6cafea2eee
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-01-26 07:12:21 -0500

    Create switch.py

---
+++ lesson3/micropython/switch.py
@@ -0,0 +1,9 @@
+import machine
+import utime
+    led_external = machine.Pin(15, machine.Pin.OUT)
+    button = machine.Pin(14, machine.Pin.IN)
+while True:
+    if button.value() == 1:
+        led_external.value(1)
+        utime.sleep(2)
+    led_external.value(0)
