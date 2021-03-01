95560821198f04e8ef48dbf7fe137f14411772ec
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-01 11:17:09 -0500

    Update date_example.py

--- lesson3/date_example.py
+++ lesson3/date_example.py
@@ -5,8 +5,8 @@ print('Date: ' + now.strftime('%m-%d-%y'))
 print('Day of Week: ' + now.strftime('%A'))
 print('Month: ' + now.strftime('%B'))
 print('Year: ' + now.strftime('%Y'))
-first = date(2020, 8, 31)
-last = date(2020, 12, 7)
+first = date(2021, 2, 1)
+last = date(2021, 5, 7)
 timediff = now - first
 print('{:d} days after the first day of classes'.format(timediff.days))
 timediff = last - now
