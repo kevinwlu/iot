318a27a4b9477e11c1e3269f080807c0815f6137
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-14 15:06:55 -0400

    Update subdata.py

--- lesson5/subdata.py
+++ lesson5/subdata.py
@@ -7,5 +7,5 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("mqtt.eclipse.org", 1883, 60)
+client.connect("mqtt.eclipseprojects.io", 1883, 60)
 client.loop_forever()
