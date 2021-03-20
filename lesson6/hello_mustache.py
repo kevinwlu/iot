bb3de751f466502e6af76db834c4e4c16aa38d4c
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-20 19:29:03 -0400

    Create hello_mustache.py

---
+++ lesson6/hello_mustache.py
@@ -0,0 +1,15 @@
+import pystache
+print(pystache.render('Hi {{person}}!', {'person': 'Mom'}))
+
+class SayHello(object):
+    def to(self):
+        return "Pizza"
+
+hello = SayHello()
+renderer = pystache.Renderer()
+print(renderer.render(hello))
+
+parsed = pystache.parse(u"Hey {{#who}}{{.}}!{{/who}}")
+print(parsed)
+print(renderer.render(parsed, {'who': 'Pops'}))
+print(renderer.render(parsed, {'who': 'you'}))
