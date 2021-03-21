325ca52507690b341a8cf8a590582a422eb0ff52
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-20 22:15:51 -0400

    Update say_hello.py

--- lesson6/say_hello.py
+++ lesson6/say_hello.py
@@ -13,7 +13,7 @@ renderer = pystache.Renderer()
 print(renderer.render(hello))
 
 # 
-parsed = pystache.parse("Hey {{#who}}{{.}}!{{/who}}")
+parsed = pystache.parse('Hey {{#who}}{{.}}!{{/who}}')
 print(parsed)
 print(renderer.render(parsed, {'who': 'Google'}))
 print(renderer.render(parsed, {'who': 'Pi'}))
