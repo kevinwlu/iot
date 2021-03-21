7bea505d6dfcdcc1804e46a85012dbaf6c3850f2
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-21 17:09:09 -0400

    Update say_hello.py

--- lesson6/say_hello.py
+++ lesson6/say_hello.py
@@ -5,7 +5,7 @@ print(pystache.render('Hi {{person}}!', {'person': 'Alexa'}))
 # Create dedicated view classes to hold view logic
 class SayHello(object):
     def to(self):
-        return "Pizza"
+        return "World"
 hello = SayHello()
 
 # Use template in say_hello.mustache
@@ -16,4 +16,4 @@ print(renderer.render(hello))
 parsed = pystache.parse('Hey {{#who}}{{.}}!{{/who}}')
 print(parsed)
 print(renderer.render(parsed, {'who': 'Google'}))
-print(renderer.render(parsed, {'who': 'Pi'}))
+print(renderer.render(parsed, {'who': 'Siri'}))
