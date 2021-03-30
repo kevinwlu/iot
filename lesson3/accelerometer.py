0849997938824a7f9a42f0cc094b5c7a81215ef9
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-30 15:11:10 -0400

    Create accelerometer.py

---
+++ lesson3/accelerometer.py
@@ -0,0 +1,12 @@
+# https://pimylifeup.com/raspberry-pi-accelerometer-adxl345/
+import time
+import board
+import busio
+import adafruit_adxl34x
+
+i2c = busio.I2C(board.SCL, board.SDA)
+accelerometer = adafruit_adxl34x.ADXL345(i2c)
+
+while True:
+    print("%f %f %f"%accelerometer.acceleration)
+    time.sleep(1)
