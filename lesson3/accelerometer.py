fa08bade8e9a0e2aa65bcf8b75199cf37fa6d196
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-30 15:15:22 -0400

    Update accelerometer.py

--- lesson3/accelerometer.py
+++ lesson3/accelerometer.py
@@ -1,4 +1,7 @@
 # https://pimylifeup.com/raspberry-pi-accelerometer-adxl345/
+# sudo apt-get install python3-dev python3-pip python3-smbus i2c-tools -y
+# sudo i2cdetect -y 1
+# sudo pip3 install adafruit-circuitpython-ADXL34x
 import time
 import board
 import busio
