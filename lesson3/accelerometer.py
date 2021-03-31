0f140b624a889eb483fd798c2815d4945196fda5
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-03-31 17:56:42 -0400

    Update accelerometer.py

--- lesson3/accelerometer.py
+++ lesson3/accelerometer.py
@@ -11,7 +11,14 @@ import adafruit_adxl34x
 
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
+accelerometer.enable_freefall_detection(threshold=10, time=25)
+accelerometer.enable_motion_detection(threshold=18)
+accelerometer.enable_tap_detection(tap_count=1, threshold=20, duration=50, latency=20, window=255)
 
 while True:
     print("%f %f %f"%accelerometer.acceleration)
+    print("Dropped: %s"%accelerometer.events["freefall"])
+    print("Tapped: %s"%accelerometer.events['tap'])
+    print("Motion detected: %s"%accelerometer.events['motion'])
     time.sleep(1)
+    
