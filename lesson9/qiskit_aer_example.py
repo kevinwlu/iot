146f324b1a053678c798a18e8aa0289154735d10
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-04-11 23:03:22 -0400

    Update qiskit_aer_example.py

--- lesson9/qiskit_aer_example.py
+++ lesson9/qiskit_aer_example.py
@@ -4,7 +4,7 @@ from qiskit.providers.aer.noise import NoiseModel
 
 # Choose a real device to simulate from IBMQ provider
 provider = IBMQ.load_account()
-backend = provider.get_backend('ibmq_valencia')
+backend = provider.get_backend('ibmq_lima')
 coupling_map = backend.configuration().coupling_map
 
 # Generate an Aer noise model for device
