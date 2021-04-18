4a0304b8dd80c7fc796594f25003c80912b08861
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-04-17 22:24:14 -0400

    Update dht_simpletest.py

--- lesson7/dht_simpletest.py
+++ lesson7/dht_simpletest.py
@@ -1,15 +1,18 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 import time
 import board
 import adafruit_dht
- 
+
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D24)
- 
+dhtDevice = adafruit_dht.DHT22(board.D18)
+
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
 # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
- 
+
 while True:
     try:
         # Print the values to the serial port
@@ -21,7 +24,7 @@ while True:
                 temperature_f, temperature_c, humidity
             )
         )
- 
+
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
