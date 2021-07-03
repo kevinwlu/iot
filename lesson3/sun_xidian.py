4aca6352346855bbfced5c9a39e6e2de92788fdd
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-03 16:41:03 -0400

    Update sun_xidian.py

--- lesson3/sun_xidian.py
+++ lesson3/sun_xidian.py
@@ -3,7 +3,7 @@ from astral.geocoder import database, lookup
 from astral.sun import sun
 from astral import LocationInfo
 l = LocationInfo('Xidian', 'China', 'Asia/Shanghai', 34.12250, 108.84029)
-s = sun(l.observer, date=date.today())
+s = sun(l.observer, date=date.today(), tzinfo=l.timezone)
 print('Dawn:    %s' % str(s['dawn']))
 print('Sunrise: %s' % str(s['sunrise']))
 print('Noon:    %s' % str(s['noon']))
