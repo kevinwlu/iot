79ce16d432f68b50c4de06e4412b66a75d3970ae
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-03 16:37:39 -0400

    Update sun_xidian.py

--- lesson3/sun_xidian.py
+++ lesson3/sun_xidian.py
@@ -2,9 +2,8 @@ from datetime import date
 from astral.geocoder import database, lookup
 from astral.sun import sun
 from astral import LocationInfo
-l = LocationInfo(('Xidian', 'China',  34.12250,108.84029, 'Asia/Shanghai', 0))
- # Record the location of Xichong, pay attention to the longitude first and then the latitude
-s = l.sun(date=date.today(),local=True)
+l = LocationInfo('Xidian', 'China', 'Asia/Shanghai', 34.12250, 108.84029)
+s = sun(l.observer, date=date.today())
 print('Dawn:    %s' % str(s['dawn']))
 print('Sunrise: %s' % str(s['sunrise']))
 print('Noon:    %s' % str(s['noon']))
