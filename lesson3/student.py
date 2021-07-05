32ade93bfa148c0b1b92dfab266f2b9f0693d8db
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-05 18:05:08 -0400

    Update student.py

--- lesson3/student.py
+++ lesson3/student.py
@@ -5,10 +5,10 @@ def averageGrade(students):
     average=sum/len(students)
     return average
 def printRecords(students):
-    print('There are %d students' % (len(students)))
+    print('There are {:d} students'.format(len(students)))
     i=1
     for key in students:
-        print('Student %d:' % (i))
-        print('Name: ' + students[key]['name'])
-        print('Grade: ' + str(students[key]['grade']))
+        print('Student {:d}:'.format(i))
+        print('Name:', students[key]['name'])
+        print('Grade:', str(students[key]['grade']))
         i=i+1
