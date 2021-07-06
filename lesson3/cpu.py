60e6a4e998f4eca644c663ec194ea96f293572cd
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-06 14:54:40 -0400

    Update cpu.py

--- lesson3/cpu.py
+++ lesson3/cpu.py
@@ -1,10 +1,14 @@
+# https://pypi.org/project/psutil/
 import psutil
-# Return the number of logical CPUs in the system
-log = psutil.cpu_count()
-print('The number of logical CPUs = ', log)
+
 # Return the number of physical cores only
 phy = psutil.cpu_count(logical=False)
 print('The number of physical cores = ', phy)
+
+# Return the number of logical CPUs in the system
+log = psutil.cpu_count()
+print('The number of logical CPUs = ', log)
+
 # Returns a list of floats representing the utilization as a percentage for each CPU
 print('The utilization per second as a percentage for each CPU')
 for i in range(10):
