eb8a83ae657753ae8cd9512029e799c607e64183
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-06 13:14:43 -0400

    Update cpu.py

--- lesson3/cpu.py
+++ lesson3/cpu.py
@@ -5,10 +5,10 @@ print('The number of logical CPUs = ', log)
 # Return the number of physical cores only
 phy = psutil.cpu_count(logical=False)
 print('The number of physical cores = ', phy)
-# Return hardware temperatures
-tmp = psutil.temperatures()
-print('Hardware emperatures in degrees Celsius')
-print(tmp)
+# Return hardware sensors
+sen = psutil.sensors()
+print('Hardware sensors')
+print(sen)
 # Returns a list of floats representing the utilization as a percentage for each CPU
 print('The utilization as a percentage for each CPU')
 for i in range(10):
