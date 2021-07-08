4875bc982438644c9ce758baa57cb07142144a39
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-08 14:32:20 -0400

    Create subxidian.py

---
+++ lesson5/subxidian.py
@@ -0,0 +1,11 @@
+import paho.mqtt.client as mqtt
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("Xidian")
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload.decode()))
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("mqtt.eclipseprojects.io", 1883, 60)
+client.loop_forever()
