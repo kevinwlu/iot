a1160f15dcbb87ef11af158ca39e5b34f9ce8513
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-08 14:30:33 -0400

    Create pubcpu.py

---
+++ lesson5/pubcpu.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+import psutil
+import paho.mqtt.client as mqtt
+mqttc = mqtt.Client()
+mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
+mqttc.loop_start()
+while True:
+    try:  
+        cpu = psutil.cpu_percent() 
+        if cpu is None:
+            time.sleep(2)
+            continue
+        now = datetime.datetime.now()
+        dt = now.replace(microsecond=0)
+        print(dt)
+        print('CPU Usage:   {0:0.1f} %'.format(cpu))
+        mqttc.publish("CPU", "%s" % dt)
+        mqttc.publish("CPU", "CPU Usage:   %.1f %%" % cpu)
+        time.sleep(10)
+    except KeyboardInterrupt:
+        exit()
