d545c71f9746cfe5151fa27228e5b5ab15561aac
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-09 22:55:42 -0400

    Create views.py

---
+++ lesson4/mycpu/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from myapp.models import LocationData, Dt, Mem, Cpu
+from rest_framework import viewsets
+from django.template import RequestContext
+from myapp.serializers import DtSerializer, MemSerializer, CpuSerializer
+import requests
+import json
+
+# Create your views here.
+class DtViewSet(viewsets.ModelViewSet):
+    queryset = Dt.objects.all()
+    serializer_class = DtSerializer
+
+class MemViewSet(viewsets.ModelViewSet):
+    queryset = Mem.objects.all()
+    serializer_class = MemSerializer
+
+class CpuViewSet(viewsets.ModelViewSet):
+    queryset = Cpu.objects.all()
+    serializer_class = CpuSerializer
+
+def home(request):
+    locData = LocationData.objects.order_by('-id')[0]
+    lat = locData.latitude
+    lon = locData.longitude
+
+    dtstate = '2021-07-07'
+    r = requests.get('http://127.0.0.1:8000/dt/1/', auth=('admin', 'admin'))
+    result = r.text
+    output = json.loads(result)
+    dtstate = output['name']
+
+    memstate = '20'
+    r = requests.get('http://127.0.0.1:8000/mem/1/', auth=('admin', 'admin'))
+    result = r.text
+    output = json.loads(result)
+    memstate = output['name']
+
+    cpustate = '50'
+    r = requests.get('http://127.0.0.1:8000/cpu/1/', auth=('admin', 'admin'))
+    result = r.text
+    output = json.loads(result)
+    cpustate = output['name']
+
+    return render(request, 'myapp/index.html',
+    {'lat': lat, 'lon': lon, 'dtstate':dtstate, 'memstate':memstate, 'cpustate':cpustate})
