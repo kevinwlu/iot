0848cdb70fc77713c716746c73af7acaa04d5a91
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-12 18:44:57 -0400

    Update keras_diabetes.py

--- lesson8/keras_diabetes.py
+++ lesson8/keras_diabetes.py
@@ -1,7 +1,7 @@
 # https://machinelearningmastery.com/tutorial-first-neural-network-python-keras/
 # Create your first MLP in Keras
-from keras.models import Sequential
-from keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 import numpy
 # fix random seed for reproducibility
 numpy.random.seed(7)
