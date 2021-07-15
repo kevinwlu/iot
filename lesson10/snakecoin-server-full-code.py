a3c387202be72c674a6058af17b21602b334d681
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-15 16:21:47 -0400

    Update snakecoin-server-full-code.py

--- lesson10/snakecoin-server-full-code.py
+++ lesson10/snakecoin-server-full-code.py
@@ -171,4 +171,10 @@ def mine():
       "hash": last_block_hash
   }) + "\n"
 
+@node.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', 
+                               mimetype='image/vnd.microsoft.icon')
+
 node.run()
