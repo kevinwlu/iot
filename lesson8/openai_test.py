2cf94abd950c4b88eeb4cb57ee1fb62a8a7d75cc
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-07-22 15:19:57 -0400

    Update openai_test.py

--- lesson8/openai_test.py
+++ lesson8/openai_test.py
@@ -1,7 +1,6 @@
-import os
 import openai
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "OPENAI_API_KEY"
 
 response = openai.Completion.create(engine="davinci", prompt="This is a test", max_tokens=5)
