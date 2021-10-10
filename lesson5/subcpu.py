0df9c008fc29e8f7732c695a54fe0aa0f76fd31e
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2021-10-10 07:06:02 -0400

    Update and rename subxidian.py to subcpu.py

--- lesson5/subcpu.py
+++ lesson5/subcpu.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("Xidian")
+    client.subscribe("MyCPU")
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload.decode()))
 client = mqtt.Client()
