2981db4c31cd92f3a4554699c2499b8116e04f6d
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2022-06-30 15:04:54 -0400

    Update IMU_Capture.ino

--- lesson6/xiao/IMU_Capture.ino
+++ lesson6/xiao/IMU_Capture.ino
@@ -4,8 +4,7 @@
 //Create a instance of class LSM6DS3
 LSM6DS3 myIMU(I2C_MODE, 0x6A);    //I2C device address 0x6A
 float aX, aY, aZ, gX, gY, gZ;
-const float accelerationThreshold = 2.5; // threshold of significant in 
-G's
+const float accelerationThreshold = 2.5; // threshold of significant in G's
 const int numSamples = 119;
 int samplesRead = numSamples;
 
