0222a69fd192596f10a57b68db246a9644738c0b
Kevin W. Lu, D.Sc <kevin.wen.lu@gmail.com>
2022-10-17 21:20:16 -0400

    Update subraspi.py

--- lesson5/subraspi.py
+++ lesson5/subraspi.py
@@ -7,5 +7,6 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("mqtt.eclipseprojects.io", 1883, 60)
+#client.connect("mqtt.eclipseprojects.io", 1883, 60)
+client.connect("test.mosquitto.org", 1883, 60)
 client.loop_forever()
