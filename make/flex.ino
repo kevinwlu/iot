5a5fa7df77f87eb65d6284472e6d8ea19a3dfd0c
Kevin Lu <kevin.wen.lu@gmail.com>
2023-04-04 16:15:24 -0400

    Create flex.ino

---
+++ make/flex.ino
@@ -0,0 +1,31 @@
+/* How to use a flex sensor/resistro - Arduino Tutorial
+   Fade an LED with a flex sensor
+   More info: http://www.ardumotive.com/how-to-use-a-flex-sensor-en.html
+   Dev: Michalis Vasilakis // Date: 9/7/2015 // www.ardumotive.com  */
+   
+
+//Constants:
+const int ledPin = 3;   //pin 3 has PWM funtion
+const int flexPin = A0; //pin A0 to read analog input
+
+//Variables:
+int value; //save analog value
+
+
+void setup(){
+  
+  pinMode(ledPin, OUTPUT);  //Set pin 3 as 'output'
+  Serial.begin(9600);       //Begin serial communication
+
+}
+
+void loop(){
+  
+  value = analogRead(flexPin);         //Read and save analog value from potentiometer
+  Serial.println(value);               //Print value
+  value = map(value, 700, 900, 0, 255);//Map value 0-1023 to 0-255 (PWM)
+  analogWrite(ledPin, value);          //Send PWM value to led
+  delay(100);                          //Small delay
+  
+}
+
