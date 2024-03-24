306fbe22cf2ef3cf06da4e2e80d025af4b9f9490
Kevin Lu <kevin.wen.lu@gmail.com>
2024-03-24 14:39:41 -0400

    Update client.py

--- lesson5/client.py
+++ lesson5/client.py
@@ -11,8 +11,8 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload.decode()))
 
-client = mqtt.Client()
-#client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+#client = mqtt.Client()
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
 #client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_message = on_message
